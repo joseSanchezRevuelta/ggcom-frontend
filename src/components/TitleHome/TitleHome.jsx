e27c24ca375d21f4227cebc5016bb9e6f293a18e
joseSanchezRevuelta <jose_mantecas@hotmail.com>
2024-03-14 01:00:37 +0100

    14-03-24  responsive home

--- src/components/TitleHome/TitleHome.jsx
+++ src/components/TitleHome/TitleHome.jsx
@@ -2,13 +2,14 @@
 function TitleHome({ title, subtitle, subtitle2 }) {
     return (
         <>
-            <div className='text-center lg:w-4/5 mx-auto mt-28'>
+            <div className='text-center lg:w-4/5 mx-auto mt-32'>
+            <span className="pt-7 text-5xl font-mantinia text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-main">{title}</span></span>
                 <img
-                    className="h-16 w-auto hover:border border-transparent mx-auto mb-4"
+                    className="h-16 w-auto hover:border border-transparent mx-auto mb-4 mt-4"
                     src="/img/logo_sf.png"
                     alt="Your Company"
                 />
-                <span className="pt-7 text-5xl font-mantinia text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-main">{title}</span></span>
+
                 <p className="mt-2 text-2xl font-mantinia text-main lg:text-xl dark:text-main">{subtitle}</p>
                 <p className="text-lg font-mantinia text-main lg:text-xl dark:text-main">{subtitle2}</p>
             </div>
