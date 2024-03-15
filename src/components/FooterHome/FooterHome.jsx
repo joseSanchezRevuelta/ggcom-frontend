3819258cbcfd7863e4d872385e1dee726cfde192
joseSanchezRevuelta <jose_mantecas@hotmail.com>
2024-03-15 23:30:05 +0100

    arreglo de home

--- src/components/FooterHome/FooterHome.jsx
+++ src/components/FooterHome/FooterHome.jsx
@@ -2,7 +2,7 @@
 function FooterHome() {
     return (
         <>
-            <div className='text-center mx-auto mt-40 lg:mt-38 flex flex-row w-full'>
+            <div className='text-center mx-auto mt-40 lg:mt-32 flex flex-row w-full'>
                 <div className="mx-auto flex flex-row items-center">
                 <span className="text-white">Colaborating with</span>
                 <a href="https://rawg.io/apidocs" target="_blank" rel="noopener noreferrer"><img
